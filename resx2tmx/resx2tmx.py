09d58fedf0e152b7a111a17d9d39286606e9e3ff
Joss Smith <JSmith@mii.com>
2017-03-16 17:46:41 +0000

    Template is now populated with dummy data using chameleon and output to console.

--- resx2tmx/resx2tmx.py
+++ resx2tmx/resx2tmx.py
@@ -1,5 +1,30 @@
 import sys
 import os.path
+from chameleon import PageTemplateLoader
+from datetime import datetime
+
+""" Resx2tmx
+This converter relies on Chameleon. See: https://pypi.python.org/pypi/Chameleon
+Installed with python -m pip install Chameleon
+"""
+
+RESX2TMX_VERSION = 0.2
+TEMPLATE_DIR = "templates"
+TEMPLATE = "template.tmx"
+TIMESTAMP_JSON_FORMAT = "%Y%m%dT%H%M%SZ"
+
+class Document:
+    def __init__(self, target_doc):
+        self.datetime = datetime.now().strftime(TIMESTAMP_JSON_FORMAT)
+        self.target_doc = os.path.abspath(target_doc)
+        self.tool_version = RESX2TMX_VERSION
+
+class TranslationItem:
+    def __init__(self, src_lang, src_text, target_lang, target_text):
+        self.source_lang = src_lang
+        self.source_text = src_text
+        self.target_lang = target_lang
+        self.target_text = target_text
 
 
 def main(target_lang_resx, out_tmx):
@@ -10,7 +35,16 @@ def main(target_lang_resx, out_tmx):
         print("File '{}' does not exist.".format(target_lang_resx))
         return
 
+    templates = PageTemplateLoader(os.path.abspath(TEMPLATE_DIR))
+    tmx_template = templates[TEMPLATE]
 
+    doc = Document(target_lang_resx)
+    # todo parse the resx files :)
+    item1 = TranslationItem("en","hello","fr","bonjour") 
+    item2 = TranslationItem("en","what","fr","quoi") 
+    trans_units = [item1, item2]
+
+    print (tmx_template(items=trans_units, document = doc))
 
 
 if __name__ == "__main__":
