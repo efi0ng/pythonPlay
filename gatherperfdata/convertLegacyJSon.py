b19f0bb5a4bcff26a93773f5b2653e7e42e1af01
efi0ng <git@efi0ng.com>
2017-03-17 00:13:20 +0000

    added treatment for build number ommission

--- gatherperfdata/convertLegacyJSon.py
+++ gatherperfdata/convertLegacyJSon.py
@@ -1,8 +1,30 @@
+#!/usr/bin/env python3
+
 import sys
 import os.path
 import json
 from gatherperfdata import JSonLabels, OpLabels
+import re
 
+def treat_build_number(data, source_file):
+    """Look for and correct build version missing."""
+    build_tested = data[JSonLabels.BUILD_TESTED]
+    if (build_tested[JSonLabels.REVISION] > 0
+        and build_tested[JSonLabels.VERSION_SHORT] != ""):
+        return
+
+    REGEX = r"r([0-9]+).*?v([0-9.]+)"
+    match = re.search(REGEX, source_file) 
+    if match is None:
+        print("Could not fix {}".format(source_file), sys.stderr)
+        return
+
+    rev = int(match.group(1))
+    ver = match.group(2)
+    build_tested[JSonLabels.REVISION] = rev
+    build_tested[JSonLabels.VERSION_SHORT] = ver
+    build_tested[JSonLabels.VERSION_LONG] = ver
+    
 
 def main(source_path, target_path):
     # todo: write intelligent code here
@@ -27,9 +49,14 @@ def main(source_path, target_path):
         target_filepath = os.path.join(target_path, source_file)
         with open(current_filepath, 'r') as f:
             data = json.load(f)
-            print(data[JSonLabels.BUILD_TESTED])
 
+        treat_build_number(data, source_file)
 
+        print(data[JSonLabels.BUILD_TESTED])
+        with open(target_filepath, 'w') as f:
+            json.dump(data, f, indent=3, sort_keys=True)
+
+            
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: {} <source_path> <target_path>".format(sys.argv[0]))
