3f6741c8970d45a667ceadbd0b82067a794ed7e5
efi0ng <git@efi0ng.com>
2019-04-14 12:20:10 +0100

    More refactoring towards writing deovr files

--- scripts/make_deovr_data.py
+++ scripts/make_deovr_data.py
@@ -9,13 +9,6 @@ import re
 
 # Path parts!
 
-_ROOT_DIR_WIN = Path("N:/vr")
-_ROOT_DIR_LINUX = Path("~/mnt/oook/vr").expanduser()
-
-_ROOT_DIR = _ROOT_DIR_LINUX if os.name == "posix" else _ROOT_DIR_WIN
-
-_BASE_URL = "http://192.168.0.35/vr/"
-
 
 class TimeStamp:
     def __init__(self, seconds: int, name: str):
@@ -70,7 +63,11 @@ class TimeCode:
         return False
 
     @staticmethod
-    def duration(time: str):
+    def duration(time: Any):
+        # if duration was specified as seconds it may already be an int
+        if type(time) is int:
+            return time
+
         for regex in TimeCode._REGEX:
             if re.fullmatch(regex[0], time):
                 return regex[1](time)
@@ -192,6 +189,8 @@ class VrDescLabels:
     GROUP = "group"
     ACTORS = "actors"
     TIME_STAMPS = "timeStamps"
+    TS_TIMECODE = "ts"
+    TS_NAME = "name"
 
 
 class VrVideoDesc:
@@ -201,12 +200,16 @@ class VrVideoDesc:
     PREVIEW_SUFFIX = "_preview.mp4"
     DEOVR_EXT = ".deovr"
 
-    def __init__(self, descriptor: Path, title: str, group: str, video_url: str, thumb_url: str, deovr_url: str, duration: int=0):
+    def __init__(self, descriptor: Path,
+                 parent_path: str, parent_url: str,
+                 title: str, group: str,
+                 video_url: str, thumb_url: str, duration: int = 0):
         self.descriptor: Path = descriptor
+        self._parent_url: str = parent_url
+        self._parent_path: str = parent_path
         self.title: str = title
         self.video_url: str = video_url
         self.thumb_url: str = thumb_url
-        self.deovr_url: str = deovr_url
         self.preview_url: Optional[str] = None
         self.seek_url: Optional[str] = None
         self.time_stamps: Any = None
@@ -216,9 +219,20 @@ class VrVideoDesc:
     @property
     def group(self): return self._group
 
-    def get_deovr_json(self) -> object:
+    @property
+    def name_stem(self): return self.descriptor.stem
+
+    @property
+    def parent_path(self): return self._parent_path
+
+    @property
+    def parent_url(self): return self._parent_url
+
+    def get_deovr_obj(self) -> object:
         """Produce json object that represents video description file"""
-        deovr = DeoVrVideo(self.title, self.video_url, self.thumb_url, self.deovr_url)
+        deovr_url = self.parent_url + self.name_stem + VrVideoDesc.DEOVR_EXT
+
+        deovr = DeoVrVideo(self.title, self.video_url, self.thumb_url, deovr_url)
         if self.preview_url:
             deovr.set_preview(self.preview_url)
 
@@ -229,59 +243,67 @@ class VrVideoDesc:
             deovr.set_duration(self.duration)
 
         if self.time_stamps:
-            # todo: parse timestamps for timecodes.
-            deovr.set_time_stamps_raw(self.time_stamps)
+            deovr.set_time_stamps(self.time_stamps)
 
-        return deovr.get_video_json()
+        return deovr
 
-    @staticmethod
-    def load(desc_path: Path) -> object:
-        file = None
-        try:
-            file = desc_path.open(mode="r", encoding="utf-8")
-            relative_path = desc_path.relative_to(_ROOT_DIR)
-            desc_stem = relative_path.stem
-            thumb_file = relative_path.with_suffix(VrVideoDesc.THUMB_EXT)
-            thumb_url = _BASE_URL + str(thumb_file.as_posix())
-            deovr_file = relative_path.with_suffix(VrVideoDesc.DEOVR_EXT)
-            deovr_url = _BASE_URL + str(deovr_file.as_posix())
 
-            desc_json = json.load(file)
-            title = desc_json[VrDescLabels.TITLE]
-            video = desc_json[VrDescLabels.VIDEO]
-            group = desc_json[VrDescLabels.GROUP]
+def load_video(desc_path: Path, root_dir: Path, base_url: str) -> VrVideoDesc:
+    def parse_time_stamps(ts_json):
+        time_stamps = []
+        for stamp in ts_json:
+            name = stamp[VrDescLabels.TS_NAME]
+            duration = TimeCode.duration(stamp[VrDescLabels.TS_TIMECODE])
+            time_stamps.append(TimeStamp(name, duration))
 
-            video_url = _BASE_URL + relative_path.with_name(video).as_posix()
+        return time_stamps
 
-            vid_desc = VrVideoDesc(desc_path, title, group, video_url, thumb_url, deovr_url)
+    file = None
+    try:
+        file = desc_path.open(mode="r", encoding="utf-8")
+        parent_path = desc_path.parent
+        relative_path = desc_path.relative_to(root_dir)
+        parent_url = base_url + str(relative_path.parent.as_posix())
 
-            preview_file = desc_stem + VrVideoDesc.PREVIEW_SUFFIX
-            if desc_path.with_name(preview_file).exists():
-                preview_url = _BASE_URL + str(relative_path.with_name(preview_file).as_posix())
-                vid_desc.preview_url = preview_url
+        thumb_file = relative_path.with_suffix(VrVideoDesc.THUMB_EXT)
+        thumb_url = base_url + str(thumb_file.as_posix())
 
-            seek_file = desc_stem + VrVideoDesc.SEEK_SUFFIX
-            if desc_path.with_name(seek_file).exists():
-                seek_url = _BASE_URL + str(relative_path.with_name(seek_file).as_posix())
-                vid_desc.seek_url = seek_url
+        desc_json = json.load(file)
+        title = desc_json[VrDescLabels.TITLE]
+        video = desc_json[VrDescLabels.VIDEO]
+        group = desc_json[VrDescLabels.GROUP]
 
-            if VrDescLabels.TIME_STAMPS in desc_json:
-                vid_desc.time_stamps = desc_json[VrDescLabels.TIME_STAMPS]
+        video_url = base_url + relative_path.with_name(video).as_posix()
 
-            if VrDescLabels.DURATION in desc_json:
-                duration_str = desc_json[VrDescLabels.DURATION]
-                if TimeCode.is_valid(duration_str):
-                    vid_desc.duration = TimeCode.duration(duration_str)
-                else:
-                    print("{} has invalid time code:{}".format(desc_path,duration_str))
+        vid_desc = VrVideoDesc(desc_path, str(parent_path), parent_url, title, group, video_url, thumb_url)
 
-            # DIAGNOSTIC print(json.dumps(vid_desc.get_deovr_json(), indent=3))
+        preview_file = vid_desc.name_stem + VrVideoDesc.PREVIEW_SUFFIX
+        if desc_path.with_name(preview_file).exists():
+            preview_url = base_url + str(relative_path.with_name(preview_file).as_posix())
+            vid_desc.preview_url = preview_url
 
-        finally:
-            if file:
-                file.close()
+        seek_file = vid_desc.name_stem + VrVideoDesc.SEEK_SUFFIX
+        if desc_path.with_name(seek_file).exists():
+            seek_url = base_url + str(relative_path.with_name(seek_file).as_posix())
+            vid_desc.seek_url = seek_url
 
-        return vid_desc
+        if VrDescLabels.TIME_STAMPS in desc_json:
+            vid_desc.time_stamps = parse_time_stamps(desc_json[VrDescLabels.TIME_STAMPS])
+
+        if VrDescLabels.DURATION in desc_json:
+            duration_str = desc_json[VrDescLabels.DURATION]
+            if TimeCode.is_valid(duration_str):
+                vid_desc.duration = TimeCode.duration(duration_str)
+            else:
+                print("{} has invalid time code:{}".format(desc_path,duration_str))
+
+        # DIAGNOSTIC print(json.dumps(vid_desc.get_deovr_json(), indent=3))
+
+    finally:
+        if file:
+            file.close()
+
+    return vid_desc
 
 
 class VideoLibrary:
@@ -289,13 +311,14 @@ class VideoLibrary:
     Provides services to output static references for deovr."""
     _DESCRIPTOR_SUFFIX = ".desc"
 
-    def __init__(self, root_dir: Path):
+    def __init__(self, root_dir: Path, base_url):
         self.video_dict = {}
         self.root_dir: Path = root_dir
+        self.base_url: str = base_url
 
     def add_video(self, desc_path: Path):
         print("Processing {}".format(desc_path.relative_to(self.root_dir).as_posix()))
-        video_desc = VrVideoDesc.load(desc_path)
+        video_desc = load_video(desc_path, self.root_dir, self.base_url)
 
         group = video_desc.group
         if group not in self.video_dict:
@@ -309,9 +332,8 @@ class VideoLibrary:
 
     def scan_for_videos(self):
         print("Scanning directories starting at {}".format(self.root_dir))
-        # vrCatalog = DeoVrCatalog()
 
-        for root, dirs, files in os.walk(self.root_dir):
+        for root, dirs, files in os.walk(str(self.root_dir)):
             descriptors = filter(VideoLibrary.is_descriptor_file, files)
             for desc in descriptors:
                 self.add_video(Path(root, desc))
@@ -324,11 +346,19 @@ class VideoLibrary:
 
 
 def main():
+    _ROOT_DIR_WIN = Path("N:/vr")
+    _ROOT_DIR_LINUX = Path("~/mnt/oook/vr").expanduser()
+
+    _ROOT_DIR = _ROOT_DIR_LINUX if os.name == "posix" else _ROOT_DIR_WIN
+
+    # base URL must include final backslash to work at the moment
+    _BASE_URL = "http://192.168.0.35/vr/"
+
     if not _ROOT_DIR.exists():
         print("Folder '{}' does not exist.".format(_ROOT_DIR))
         return
 
-    video_lib = VideoLibrary(_ROOT_DIR)
+    video_lib = VideoLibrary(_ROOT_DIR, _BASE_URL)
     video_lib.scan_for_videos()
     video_lib.deovr_write_files()
 
